5d9671cce0bcc37bb90f5af0c933792afcc682cb
andrewtzeva <andreas.tzevas@gmail.com>
2020-09-10 11:35:53 +0300

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('content')
+
+<div class="promo-box-container">
+    <a href="#" target="_blank" rel="noopener">
+       <span class="linkspanner"></span>
+     </a>
+   <p class="title"><a href="#" target="_blank" rel="noopener">Want to create an impact? <span class="lighter">Take control and see what others can't.</span></a></p>
+   <p><a class="cta-button" href="#" target="_blank" rel="noopener">Get Started</a></p>
+   <div class="right-triangle"> </div>
+</div>
+
+@endsection
